4e6afe84a16a99eb2dba7c50d4541d2d540932f1
WAHU0508 <sharongikenye@gmail.com>
2024-08-02 14:47:24 +0300

    Forms

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,37 +5,38 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
-  const [search, setSearch] = useState("")
-  const [itemList, setItemList] = useState(items) 
-
+  const [search, setSearch] = useState("");
+  const [itemList, setItemList] = useState(items);
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
   function handleSearchChange(event) {
-    setSearch(event.target.value)
+    setSearch(event.target.value);
   }
 
-  function handleItemFormsubmit(newItem) {
-    console.log("New item to add:", newItem)
-    setItemList([...itemList, newItem])
+  function handleItemFormSubmit(newItem) {
+    console.log("New item to add:", newItem);
+    setItemList([...itemList, newItem]);
   }
-  const itemsToDisplay = items.filter((item) => {
+
+  const itemsToDisplay = itemList.filter((item) => {
     const categoryMatch = selectedCategory === "All" || item.category === selectedCategory;
-    const searchMatch = !search || item.name.toLowerCase().includes(search.toLowerCase())
+    const searchMatch = !search || item.name.toLowerCase().includes(search.toLowerCase());
 
     return categoryMatch && searchMatch;
   });
 
   return (
     <div className="ShoppingList">
-      <ItemForm onItemFormSubmit={handleItemFormsubmit}/>
-      <Filter 
-      selectedCategory={selectedCategory}
-      search={search}
-      onSearchChange={handleSearchChange}
-      onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={handleItemFormSubmit} />
+      <Filter
+        selectedCategory={selectedCategory}
+        search={search}
+        onSearchChange={handleSearchChange}
+        onCategoryChange={handleCategoryChange}
+      />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
